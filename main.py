f779905d66f22f9595fd5b782e0d62ee2e7a2b82
georgia-martinez <gcm49@case.edu>
2023-04-12 23:41:22 -0400

    Priority can be set in the config

--- main.py
+++ main.py
@@ -1,43 +1,52 @@
 import pandas as pd
 import yaml
-from dataclasses import dataclass
 
-@dataclass
+def sortable_room(room, priority, orderings):
+    vals = []
+
+    for p in priority:
+        vals.append(orderings[p].index(getattr(room, p)))
+
+    return tuple(vals)
+
 class Room():
-    house: int
-    room_num: int
-    view: str
-    large: bool
-
-    @property
-    def floor(self) -> int:
-        floor = int(str(self.room_num)[0])
-        return floor
+    def __init__(self, house, room, view, large):
+        self.house = house
+        self.room_num = room
+        self.floor = int(str(self.room_num)[0])
+        self.view = view
+        self.large = large
     
     def __str__(self):
         return f"House {self.house} Room #{self.room_num} (view: {self.view}, large: {self.large})"
 
-# Read csv with the rooms
-ROOMS_PATH = "rooms.csv"
-df = pd.read_csv(ROOMS_PATH)
+if __name__ == "__main__":
+    # Read csv with the rooms
+    ROOMS_PATH = "rooms.csv"
+    df = pd.read_csv(ROOMS_PATH)
 
-# Put all of the rooms in a list
-all_rooms = []
+    # Put all of the rooms in a list
+    all_rooms = []
 
-for index, row in df.iterrows():
-    new_room = Room(row["House"], row["Room Number"], row["View"], row["Large"])
-    all_rooms.append(new_room)
+    for index, row in df.iterrows():
+        new_room = Room(row["House"], row["Room Number"], row["View"], row["Large"])
+        all_rooms.append(new_room)
 
-# Read config with the priority ordering
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+    # Read config
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
 
-house = config["house"]
-view = config["view"]
-floor = config["floor"]
+    # Sort by priority
+    priority = config["priority"]
 
-# Sort the rooms
-all_rooms.sort(key=lambda r: (-r.large, view.index(r.view), house.index(r.house), floor.index(r.floor)))
+    orderings = {
+        "house": config["house"],
+        "view": config["view"],
+        "floor": config["floor"],
+        "large": [True, False]
+    }
 
-for room in all_rooms:
-    print(room)
+    all_rooms.sort(key=lambda room: sortable_room(room, priority, orderings))
+
+    for room in all_rooms:
+        print(room)
